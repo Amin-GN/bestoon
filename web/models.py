18d81a8dd98d579d3cc401beef5f684a04ff9089
Amin.GN <Amin.gn.7@gmail.com>
2023-04-23 13:14:48 +0330

    the Expense model added

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+#from django.contrib.auth.models import User #shouldn't use User directly
+from django.conf import settings
 
 # Create your models here.
+class Expense(models.Model):
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user= models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
